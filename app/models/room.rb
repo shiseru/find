128fddaf4357e30b1c5e46d4cc865e5439e98895
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-01 18:43:38 +0900

    add message function

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   belongs_to :participant, class_name: 'User', foreign_key: 'participant_id'
+
+  has_many :messages #一つの部屋に複数のメッセージがもてる
 end
