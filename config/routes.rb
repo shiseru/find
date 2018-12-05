a5ecdb9149799234e6a61dfb843ef3acbc8d6870
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-05 17:52:46 +0900

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   
   get '/' => "home#top"
   get 'about' => "home#about"
-  # practice
-  # get 'p' => "rooms#show"
-  # 
-  # get "rooms/:id" => "rooms#show"
+  
   resources :rooms
   post "rooms/:id/create_message" => "rooms#create_message"
 
